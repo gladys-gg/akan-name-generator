d2ce1a6c33492069b0aaae71bc6b295104d7ee81
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-03-06 01:11:05 +0300

    script.js: Added functions for reading the data from the forms

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ var weekDays =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Satu
 
 
 class Birthday{
+    getDayOfWeek(){
+        const day=document.querySelector('#day').value;
+        const month=document.querySelector('#month').value;
+        const year=document.querySelector('#year').value;
+    
+    if(day==='' || month ==='' || year===''){
+    this.showAlert();
+    }
+    else  if(day <=0 || day >31||  month >12 ){
+      this.showAlert();
+        }
